d6e8da5a8bdca18b099f46d9b782610cbdbaac37
leiyouling <469082434@qq.com>
2016-06-01 15:42:55 +0800

    这是我第一次提交程序到GITHub

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by 123 on 16-6-1.
+//  Copyright (c) 2016年 Carier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by 123 on 16-6-1.
+//  Copyright (c) 2016年 Carier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
